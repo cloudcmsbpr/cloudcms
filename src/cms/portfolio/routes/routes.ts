0b04c758a277f664750db656a4d482bce8860071
Radu-Gabriel Orleanu <radu.orleanu@gmail.com>
2020-04-14 12:07:21 +0200

    pulled master into current branch, fixed unused import

--- src/cms/portfolio/routes/routes.ts
+++ src/cms/portfolio/routes/routes.ts
@@ -8,9 +8,7 @@ import {
     getTechById
 } from "../controllers/portfolioController";
 import SingleRoute from "../../util/SingleRoute";
-import AuthController from "../../shared/controllers/authController";
 import { checkJwt } from "../../shared/middleware/authMiddleware";
-import UserController from "../../shared/controllers/userController";
 
 
 // the authenticated field is from passport, we may either remove it or replace it
